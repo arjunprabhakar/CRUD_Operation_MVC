5d62556258c387e960f7b1a3bced494415bd717e
arjunpp2023a@mca.ajce.in <arjunpp2023a@mca.ajce.in>
2023-03-26 23:56:05 +0530

    first commit

---
+++ CrudOperation/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace CrudOperation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StudentDataGrid = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.phone = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.age = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.department = new System.Windows.Forms.TextBox();
+            this.delete = new System.Windows.Forms.Button();
+            this.update = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(56, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // fname
+            // 
+            this.fname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.fname.Location = new System.Drawing.Point(201, 77);
+            this.fname.Margin = new System.Windows.Forms.Padding(4);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(225, 32);
+            this.fname.TabIndex = 2;
+            this.fname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(181, 272);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 51);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StudentDataGrid
+            // 
+            this.StudentDataGrid.BackgroundColor = System.Drawing.Color.White;
+            this.StudentDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentDataGrid.Location = new System.Drawing.Point(181, 367);
+            this.StudentDataGrid.Margin = new System.Windows.Forms.Padding(4);
+            this.StudentDataGrid.Name = "StudentDataGrid";
+            this.StudentDataGrid.RowHeadersWidth = 51;
+            this.StudentDataGrid.RowTemplate.Height = 24;
+            this.StudentDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.StudentDataGrid.Size = new System.Drawing.Size(603, 170);
+            this.StudentDataGrid.TabIndex = 4;
+            this.StudentDataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.StudentDataGrid_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(473, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last name";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lname
+            // 
+            this.lname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lname.Location = new System.Drawing.Point(604, 73);
+            this.lname.Margin = new System.Windows.Forms.Padding(4);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(217, 32);
+            this.lname.TabIndex = 2;
+            this.lname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(92, 132);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 29);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Phone";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(517, 128);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 29);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Email";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // phone
+            // 
+            this.phone.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.phone.Location = new System.Drawing.Point(200, 134);
+            this.phone.Margin = new System.Windows.Forms.Padding(4);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(225, 32);
+            this.phone.TabIndex = 2;
+            this.phone.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // email
+            // 
+            this.email.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.email.Location = new System.Drawing.Point(604, 132);
+            this.email.Margin = new System.Windows.Forms.Padding(4);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(217, 32);
+            this.email.TabIndex = 2;
+            this.email.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(119, 188);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 29);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Age";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // age
+            // 
+            this.age.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.age.Location = new System.Drawing.Point(197, 191);
+            this.age.Margin = new System.Windows.Forms.Padding(4);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(225, 32);
+            this.age.TabIndex = 2;
+            this.age.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(453, 183);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 29);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Department";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // department
+            // 
+            this.department.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.department.Location = new System.Drawing.Point(604, 191);
+            this.department.Margin = new System.Windows.Forms.Padding(4);
+            this.department.Name = "department";
+            this.department.Size = new System.Drawing.Size(217, 32);
+            this.department.TabIndex = 2;
+            this.department.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.Color.Red;
+            this.delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.delete.ForeColor = System.Drawing.Color.White;
+            this.delete.Location = new System.Drawing.Point(346, 272);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(110, 51);
+            this.delete.TabIndex = 5;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = false;
+            this.delete.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // update
+            // 
+            this.update.BackColor = System.Drawing.Color.Blue;
+            this.update.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.update.ForeColor = System.Drawing.Color.White;
+            this.update.Location = new System.Drawing.Point(501, 272);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(112, 51);
+            this.update.TabIndex = 6;
+            this.update.Text = "Update";
+            this.update.UseVisualStyleBackColor = false;
+            this.update.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Reset.ForeColor = System.Drawing.Color.White;
+            this.Reset.Location = new System.Drawing.Point(658, 272);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(107, 51);
+            this.Reset.TabIndex = 7;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.StudentDataGrid);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.department);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Data";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView StudentDataGrid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox phone;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox department;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button Reset;
+    }
+}
+
---
+++ CrudOperation/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudOperation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-CCCDLDJC;Initial Catalog=Crudoperations_DB;Integrated Security=True");
+        public int Student_id;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetStudentDetails();
+        }
+
+        private void GetStudentDetails()
+        {
+            SqlCommand cmd = new SqlCommand("select * from tbl_student",con);
+            DataTable dt = new DataTable();
+
+
+            con.Open();
+
+            SqlDataReader sdr=cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            StudentDataGrid.DataSource= dt;
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IsValid())
+            {
+            SqlCommand cmd = new SqlCommand("insert into tbl_student values (@fname,@lname,@department,@age,@email,@phone)",con);
+                cmd.CommandType= CommandType.Text;
+                cmd.Parameters.AddWithValue("@fname", fname.Text);
+                cmd.Parameters.AddWithValue("@lname", lname.Text);
+                cmd.Parameters.AddWithValue("@department", department.Text);
+                cmd.Parameters.AddWithValue("@age", age.Text);
+                cmd.Parameters.AddWithValue("@email", email.Text);
+                cmd.Parameters.AddWithValue("@phone", phone.Text);
+
+
+                con.Open();
+                
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+
+                MessageBox.Show("New Student is added successfully","saved",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                GetStudentDetails();
+                ResetForm();
+
+            }
+        }
+
+        private bool IsValid()
+        {
+           if(fname.Text == String.Empty)
+            {
+                MessageBox.Show("Student name is Required","failed",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return false;   
+            }
+            return true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Student_id > 0)
+            {
+                SqlCommand cmd = new SqlCommand("delete from tbl_student where student_id=Student_id", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@ID", this.Student_id);
+
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+
+                MessageBox.Show(" Student data Deleted successfully", "deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GetStudentDetails();
+                ResetForm();
+            }
+            else
+            {
+                MessageBox.Show("Please select a student", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Student_id > 0)
+            {
+                SqlCommand cmd = new SqlCommand("update tbl_student set  fname = @fname,lname = @lname,department = @department,age = @age,email= @email,phone = @phone where student_id=Student_id", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@fname", fname.Text);
+                cmd.Parameters.AddWithValue("@lname", lname.Text);
+                cmd.Parameters.AddWithValue("@department", department.Text);
+                cmd.Parameters.AddWithValue("@age", age.Text);
+                cmd.Parameters.AddWithValue("@email", email.Text);
+                cmd.Parameters.AddWithValue("@phone", phone.Text);
+                cmd.Parameters.AddWithValue("@ID", this.Student_id);
+
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+
+                MessageBox.Show(" Student data updated successfully", "updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GetStudentDetails();
+                ResetForm();
+            }
+            else
+            {
+                MessageBox.Show("Please select a student", "updated", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+
+        }
+
+        private void ResetForm()
+        {
+            Student_id = 0;
+            fname.Clear();
+            lname.Clear();
+            department.Clear();
+            age.Clear();
+            email.Clear();
+            phone.Clear();
+
+            fname.Focus();
+        }
+
+        private void StudentDataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Student_id = Convert.ToInt32(StudentDataGrid.SelectedRows[0].Cells[0].Value);
+            fname.Text = StudentDataGrid.SelectedRows[0].Cells[1].Value.ToString();
+            lname.Text = StudentDataGrid.SelectedRows[0].Cells[2].Value.ToString();
+            department.Text = StudentDataGrid.SelectedRows[0].Cells[3].Value.ToString();
+            age.Text = StudentDataGrid.SelectedRows[0].Cells[4].Value.ToString();
+            email.Text = StudentDataGrid.SelectedRows[0].Cells[5].Value.ToString();
+            phone.Text = StudentDataGrid.SelectedRows[0].Cells[6].Value.ToString();
+
+        }
+    }
+}
